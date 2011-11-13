9d2b9a3876bd0b1bf9580a0a49031a40ffb1c008
Naitik Shah <n@daaku.org>
2011-11-13 21:55:37 +0000

    project metadata

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject user-input "1.0.3"
   :description "Transform, validate and prepare user input."
+  :author "Naitik Shah <n@daaku.org>"
+  :url "https://github.com/nshah/user-input.clj"
   :dependencies
     [[org.clojure/clojure "1.3.0"]]
   :dev-dependencies
