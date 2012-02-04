7d7493b449e10da3e0ea045511b0c87a1a68c5cd
Naitik Shah <n@daaku.org>
2012-02-04 15:54:06 -0800

    exclude clojure from dev-dependencies

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :author "Naitik Shah <n@daaku.org>"
   :url "https://github.com/nshah/user-input.clj"
   :repl-init user-input.repl
+  :exclusions [org.clojure/clojure]
   :dependencies
     [[clj-time "0.3.3"]
      [org.clojure/clojure "1.3.0"]]
   :dev-dependencies
     [[auto-reload "1.0.3"]
      [lein-marginalia "0.7.0-SNAPSHOT"]
-     [org.clojure/clojure "1.3.0"]
      [org.clojure/tools.logging "0.2.3"]
      [vimclojure/server "2.3.0-SNAPSHOT"]])
